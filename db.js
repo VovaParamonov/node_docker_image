a79bacf0ff65cdf8e9933b4dffc752864b328646
vovaparamonov <vovaparamonov2000@mail.ru>
2022-02-03 15:20:06 +0600

    - Separated envs - Modified .gitignore

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
-const MONGO_USERNAME = 'admin';
-const MONGO_PASSWORD = 'password';
-const MONGO_HOSTNAME = '127.0.0.1';
-const MONGO_PORT = '27017';
-const MONGO_DB = 'main';
+const {
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOSTNAME,
+  MONGO_PORT,
+  MONGO_DB
+} = process.env;
 
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
